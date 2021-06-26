cc1550d28c10c4a52a536be30d305d1fe1f92768
Priyanshi Nagpal <86527964+PriyanshiNagpal@users.noreply.github.com>
2021-06-26 22:59:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+
+function App() {
+  return(
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ Info.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+    <div className="note">
+      <h1>Javascript and Backend with NodeJS-7 Day Bootcamp</h1>
+      <p>Here we bring our free 7 days live BOOTCAMP with a hands-on project at the end-The Foundation of Javascript and Backend with NodeJS</p> 
+    </div>
+    <div className="note">
+    <h1>Python and Natural Language Processing-7 Day Bootcamp</h1>
+    <p>Here we bring our free 7 days live BOOTCAMP with a hands-on project at the end-The Foundation of Python and Natural Language Programming Processing</p> 
+  </div>
+  <div className="note">
+  <h1>Pyhton and Network Security-7 Day Bootcamp</h1>
+  <p>Here we bring our free 7 days live BOOTCAMP with a hands-on project at the end-The Foundation of Python and Networking Security</p> 
+</div>
+<div className="note">
+      <h1>Tableau Fundamentals-7 Day Bootcamp</h1>
+      <p>Here we bring our free 7 days live BOOTCAMP with a hands-on project at the end-Tableau Fundamentals</p> 
+    </div>
+    <div className="note">
+    <h1>Python and Data Visualization-7 Day Bootcamp</h1>
+    <p>Here we bring our free 7 days live BOOTCAMP with a hands-on project at the end-The Foundation of Python and Data Visualization</p> 
+  </div>
+  </div>
+  
+  );
+
+}
+
+export default Info;
